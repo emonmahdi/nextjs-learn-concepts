a97605af8abd181384aa1ad196c16e206190bd20
emonmahdi <developeremon1997@gmail.com>
2025-09-26 16:16:29 +0600

    connect the tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import "@/app/ui/global.css";
 export default function Home() {
   return (
     <div>
-      <h2 className="blackBg">Nextjs Dashboard</h2>
+      <h2 className="blackBg text-3xl font-bold underline">Nextjs Dashboard</h2>
     </div>
   );
 }
