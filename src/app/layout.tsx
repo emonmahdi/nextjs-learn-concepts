2edefc395a576096f423295a0e1b2add8b4424f4
emonmahdi <developeremon1997@gmail.com>
2025-09-26 16:11:55 +0600

    font and global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { inter } from "@/app/ui/fonts";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.className} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
